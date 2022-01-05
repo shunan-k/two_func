b84543aa940f2d5dc194122d3cbc1ccfbb24ab05
shunan_k <53070080+shunan-k@users.noreply.github.com>
2022-01-05 16:37:30 +0800

    Update two_func.c

--- two_func.c
+++ two_func.c
@@ -5,6 +5,14 @@ int main(void) {
     printf("I will summon the butler function.\n");
     butler();
     printf("Yes, Bring me some tea and writeable DVDs.\n");
+    /*
+    1
+    2
+    3
+    4
+    5
+    6
+    */
 
 
     return 0;
